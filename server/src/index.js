85cb32e49a8746271abccd871e49bdb82076b39b
Nishi <nishi.soni@gsfcuniversity.ac.in>
2025-08-22 12:05:55 -0400

    redploy backend cors debugg only

--- server/src/index.js
+++ server/src/index.js
@@ -21,30 +21,17 @@ const allowedOrigins = [
   "https://chat-app-nine-delta-31.vercel.app/", // your actual Vercel frontend URL
   "http://localhost:5173",                   // for local testing (optional)
 ];
-// ✅ Setup CORS
-
-// app.use(cors({
-//   origin: allowedOrigins,
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin like mobile apps or curl
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
 
 
 
